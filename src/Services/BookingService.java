f50454402d8b0ff95a088a9b58c83fdc93365ea3
Ilyas <adygamovilyas@gmail.com>
2018-04-22 00:28:37 +0300

    implemented BookingService.checkOut method

--- src/Services/BookingService.java
+++ src/Services/BookingService.java
@@ -20,10 +20,10 @@ public class BookingService {
 
     /**
      * Implements document booking logic.
-     * Depending on user type sets priority of the user in the order.
+     * Depending on user type sets the priority of the user in the order.
      *
-     * @param patron object representing booking patron
-     * @param document object representing booking document
+     * @param patron booking patron
+     * @param document booking document
      * @throws SQLException
      */
     public static void book(Patron patron, Document document) throws SQLException {
@@ -64,7 +64,46 @@ public class BookingService {
         db.getConnection().commit();
     }
 
-    public static void checkOut(Patron patron, Librarian librarian, Copy copy) {
+    /**
+     * Implements checking out logic.
+     * Depending on user type and document type sets dueDate.
+     *
+     * @param patron  checking out patron
+     * @param librarian librarian who has
+     *                  let patron to check out the document
+     * @param copy particular copy the patring is checking out
+     * @throws SQLException
+     */
+    public static void checkOut(Patron patron, Librarian librarian, Copy copy) throws SQLException {
+        String updateText = "update copies set is_checked_out = ?, " +
+                "holder_id = ?, renew_times = renew_times + 1, check_out_date = ?, due_date = ? where id = ?;";
+        PreparedStatement st = db.getConnection().prepareStatement(updateText);
 
+        LocalDate currentDate = LocalDate.now();
+
+        int dueWeeks = 3;
+        String docType = copy.getDocument().getType();
+        if (patron.getType().equals("VP")) {
+            dueWeeks = 1;
+        } else if (patron.getType().equals("instructor") ||
+                patron.getType().equals("professor") ||
+                patron.getType().equals("TA")) {
+            dueWeeks = 4;
+        } else if (docType.equals("book") &&
+                ((Book) copy.getDocument()).getBestseller()) {
+            dueWeeks = 2;
+        } else if (docType.equals("av_material") ||
+                docType.equals("journal_issue")) {
+            dueWeeks = 2;
+        }
+
+        LocalDate dueDate = currentDate.plusWeeks(dueWeeks);
+
+        st.setInt(1, 1);
+        st.setInt(2, patron.getId());
+        st.setDate(3, Date.valueOf(currentDate));
+        st.setDate(4, Date.valueOf(dueDate));
+        st.executeUpdate();
+        db.getConnection().commit();
     }
 }
