00833944f091e2db393a1c97209a00ae3176254d
Ilyas <adygamovilyas@gmail.com>
2018-04-24 13:58:47 +0300

    patron constructor bug fixed

--- src/Objects/Patron.java
+++ src/Objects/Patron.java
@@ -7,14 +7,14 @@ public class Patron extends User {
     private Collection<Document> waitingList;
     private Collection<Copy> checkedOutCopies;
 
-    public Patron(String name, String surname, String type, String phoneNumber, String address) {
-        super(name, surname, type, phoneNumber, address);
+    public Patron(String name, String surname, String type, String phoneNumber, String address, String email) {
+        super(name, surname, type, phoneNumber, address, email);
         waitingList = new ArrayList<>();
         checkedOutCopies = new ArrayList<>();
     }
 
-    public Patron(int id, String name, String surname, String phoneNumber, String address, String type) {
-        super(id, name, surname, type, phoneNumber, address);
+    public Patron(int id, String name, String surname, String phoneNumber, String address, String type, String email) {
+        super(id, name, surname, type, phoneNumber, address, email);
         waitingList = new ArrayList<>();
         checkedOutCopies = new ArrayList<>();
     }
